deda4c7b1c4738196d3f1f5087843213a1dc381f
Zach Beeby <zach@invictaprospectsgroup.com>
2022-09-21 17:02:43 +1000

    Created Icon POCO

---
+++ src/Data/Icon.cs
@@ -0,0 +1,10 @@
+﻿namespace Data;
+
+public record Icon
+{
+    public int Order { get; init; }
+
+    public string Description { get; init; } = string.Empty;
+
+    public string ClassName { get; init; } = string.Empty;
+}
